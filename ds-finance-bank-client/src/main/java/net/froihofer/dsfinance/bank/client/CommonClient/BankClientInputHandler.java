c7088a8396ec23ba9697a4aa321b990fd7a02d60
4n7oine <a.lanners@outlook.com>
2024-01-11 17:46:15 +0100

    CommonClient for Customer an Employee

---
+++ ds-finance-bank-client/src/main/java/net/froihofer/dsfinance/bank/client/CommonClient/BankClientInputHandler.java
@@ -0,0 +1,121 @@
+package net.froihofer.dsfinance.bank.client.CommonClient;
+
+import common.bankingInterface.BankingInterface;
+import common.bankingInterface.BankingInterfaceException;
+import common.dto.CustomerDTO;
+import common.dto.TradeDTO;
+import net.froihofer.util.CommonInputHandler;
+import net.froihofer.util.RmiProxyBuilder;
+
+import java.util.Scanner;
+
+class BankClientInputHandler {
+    private Scanner scanner = new Scanner(System.in);
+    private RmiProxyBuilder rmiProxyBuilder = new RmiProxyBuilder();
+    BankingInterface bankingInterface = null;
+
+    // TODO Implement all the person persistance logic
+    void searchStockByName() {
+        CommonInputHandler.searchStockByName(bankingInterface);
+    }
+
+    String getRole() {
+        return CommonInputHandler.getRole(bankingInterface);
+    }
+
+    public void login(String username, String password){
+        bankingInterface = rmiProxyBuilder.getRmiProxy(username, password);
+    }
+
+    public void addCustomer() {
+        String firstName = CommonInputHandler.getFirstName();
+        String lastName = CommonInputHandler.getLastName();
+        String address = CommonInputHandler.getAddress();
+        Integer customerNumber = CommonInputHandler.getCustomerNumber();
+
+      bankingInterface.createCustomer(firstName, lastName, address, customerNumber, null, null);
+    }
+
+    public void searchCustomer() {
+        Integer customerNumber = CommonInputHandler.getCustomerNumber();
+
+        try {
+            CustomerDTO value = bankingInterface.searchCustomer(customerNumber);
+            System.out.println(value);
+        } catch (BankingInterfaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buyStockForCustomer() {
+        System.out.println("Buying Stock: ");
+        String customerID = CommonInputHandler.getCustomerID();
+        String stockName = CommonInputHandler.getStockSymbol();
+        Double amount = CommonInputHandler.getAmount();
+
+        try {
+            bankingInterface.buySockByISIN(new TradeDTO(customerID, stockName, amount));
+        } catch (BankingInterfaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sellStockForCustomer() {
+        System.out.println("Selling Stock: ");
+        String customerID = CommonInputHandler.getCustomerID();
+        String stockName = CommonInputHandler.getStockSymbol();
+        Double amount = CommonInputHandler.getAmount();
+
+        try {
+            bankingInterface.sellStockByISIN(new TradeDTO(customerID, stockName, amount));
+        } catch (BankingInterfaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayDepotInfoOfCustomer() {
+        System.out.println("Depot Info: ");
+        String customerID = CommonInputHandler.getCustomerID();
+
+        CommonInputHandler.printDepotInfo(Integer.valueOf(customerID), bankingInterface);
+    }
+
+    public void displayInvestableVolume() {
+        System.out.println("Investible Volume: ");
+        try {
+            String volume = bankingInterface.getInvestableVolume();
+            System.out.println(volume);
+        } catch (BankingInterfaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void buyStock() {
+        System.out.println("Buying Stock: ");
+        String stock = CommonInputHandler.getStockSymbol();
+        Double amount = CommonInputHandler.getAmount();
+
+        try {
+            bankingInterface.buySockByISIN(new TradeDTO("1", stock, amount));
+        } catch (BankingInterfaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sellStock() {
+        System.out.println("Selling Stock: ");
+        String stock = CommonInputHandler.getStockSymbol();
+        Double amount = CommonInputHandler.getAmount();
+
+        try {
+            bankingInterface.sellStockByISIN(new TradeDTO("1", stock, amount));
+        } catch (BankingInterfaceException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayDepotInfo() {
+        System.out.println("Depot Info: ");
+        CommonInputHandler.printDepotInfo(1, bankingInterface);
+    }
+}
